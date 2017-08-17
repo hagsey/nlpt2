5c01b9fca37abe7475eba83986862870fefcdae4
Eric Hagemann <hagemann.eric@gmail.com>
2017-08-16 22:03:36 -0400

    shitton

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,27 @@
 $(function() {
-  $(".contact-button").on("click", function() {
-    $(".contact-modal").show();
-    $(".modal-layer").show();
-    $("body").css("overflow", "hidden");
+    $(".contact-button").on("click", function() {
+        $(".contact-modal").show();
+        $(".modal-layer").show();
+        $("body").css("overflow", "hidden");
 
-    $(".modal-layer").on("click", function() {
-      $(".contact-modal").hide();
-      $(".modal-layer").hide();
-      $("body").css("overflow", "visible");
+        $(".modal-layer").on("click", function() {
+            $(".contact-modal").hide();
+            $(".modal-layer").hide();
+            $("body").css("overflow", "visible");
+        });
     });
-  });
+
+    var x = sessionStorage.getItem("respPopUp");
+
+    if (!x) {
+      setTimeout(function() {
+          $("#email-popup").fadeIn();
+          $("body").css("overflow", "hidden");
+          sessionStorage.setItem("respPopUp", true);
+          $(".fa-times, .no-thanks, .yes-please").on("click", function() {
+              $("body").css("overflow", "scroll");
+              $("#email-popup").hide();
+          });
+      }, 7000);      
+    }
 });
